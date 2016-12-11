3da424dfead0b82648875192768343703dd3b31f
Varlen Neto <varlenneto@poli.ufrj.br>
2016-12-11 16:34:22 -0200

    Adiciona Agendamentos e Localização aos recursos

--- domain/Recurso.py
+++ domain/Recurso.py
@@ -1,12 +1,13 @@
 from .TipoRecurso import TipoRecurso
 class Recurso():
-    def __init__(self, nome=None, tipo=None, local=None):
+    def __init__(self, nome=None, tipo=None, local=None, agendamentos=[]):
         self.id = None
         self.nome = nome
-        self.localizacao = None
+        self.localizacao = local
         if not tipo:
-            tipo = TipoRecurso()
+            tipo = TipoRecurso(0)
         self.tipo = tipo
+        self.agendamentos = agendamentos
 
     def __str__(self):
         return nome
