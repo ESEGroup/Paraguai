a428814c8053e11ad32fbf0142f739a3ee26d3aa
Varlen Neto <varlenneto@poli.ufrj.br>
2016-12-11 16:42:40 -0200

    Implementa o método de busca no serviço e no Repositório em Memória

--- web/RepositorioRecursoEmMemoria.py
+++ web/RepositorioRecursoEmMemoria.py
@@ -1,19 +1,104 @@
-from domain import RepositorioRecurso, Recurso, TipoRecurso
+from difflib import SequenceMatcher
+from itertools import product
+from domain import RepositorioRecurso, Recurso, TipoRecurso, IntervaloDeTempo, Agendamento
 
 class RepositorioRecursoEmMemoria(RepositorioRecurso):
     def __init__(self):
         listaTipos = [(0,"Sala"),(1,"Projetor"),(2,"Microscópio")]
         self.listaTipoRecurso = [TipoRecurso(id = identificador, nome = recurso) for (identificador,recurso) in listaTipos ]
+        self.recursos = []
+        # Mudar depois
+        self.mockar_recursos()
 
-        teste = Recurso("Recurso Teste")
-        teste.id = 1
-        teste.tipo = self.listaTipoRecurso[0]
-        self.recursos = [teste]
+    def mockar_recursos(self):
+        recursos_iniciais = [("Sala H-201",0,"Fundão,CT,Bloco H"),
+                             ("Sala H-204",0,"Fundão,CT,Bloco H"),
+                             ("Projetor AB-Z",1,"Fundão,CT,Sala D-201"),
+                             ("Projetor AB-X",1,"Fundão,CCMN,Sala F-102"),
+                             ("Microscópio XYZ",2,"Fundão,CCMN,Sala F201")]
+        qtd_recursos = len(self.recursos)
+        for index,(nome,tipo,local) in enumerate(recursos_iniciais):
+            id = qtd_recursos + index + 1
+            novoRecurso = Recurso(nome, self.tipo_por_id(tipo), local)
+            novoRecurso.id = id
+            self.recursos.append(novoRecurso)
+
+
+        ag = []
+        natal = IntervaloDeTempo('2016-12-24 00:00','2016-12-26 00:00')
+        agNatal = Agendamento(natal)
+        ag.append(agNatal)
+        recursoComAgendamento = Recurso("Projetor Natalino",self.tipo_por_id(1),"Fundão,CCMN,Sala D-201",ag)
+        id = len(self.recursos) + 1
+        recursoComAgendamento.id = id
+        self.recursos.append(recursoComAgendamento)
+
+
 
     def criar(self, recurso):
         recurso.id = len(self.recursos) + 1
         self.recursos.append(recurso)
         return recurso
+
+    def recurso_por_id(self, id):
+        return [elemento
+                for elemento
+                in self.recursos
+                if elemento.id == id]
+
+    def buscar(self, recursoFiltro):
+        if recursoFiltro.id:
+            return self.recurso_por_id(recursoFiltro.id)
+
+        iter_recursos = iter(self.recursos)
+        # Filtro por tipo
+        print("Filtrando recursos com o tipo:", recursoFiltro.tipo.id)
+        iter_recursos = (
+                        r
+                        for r
+                        in iter_recursos
+                        if r.tipo == recursoFiltro.tipo
+                        )
+        # Busca por nome
+        if recursoFiltro.nome:
+            iter_recursos = (
+                             r
+                             for r
+                             in iter_recursos
+                             if SequenceMatcher(None, r.nome, recursoFiltro.nome).ratio() > 0.5
+                             )
+        # Filtro por localização
+        if recursoFiltro.localizacao:
+            print("Filtro por local -> ", recursoFiltro.localizacao)
+            iter_recursos = (
+                             r
+                             for r
+                             in iter_recursos
+                             #if SequenceMatcher(None, r.localizacao.split(','), recursoFiltro.localizacao.split(',')).ratio() > 0.51
+                             if self.proximidade(r.localizacao, recursoFiltro.localizacao) > 0.6
+                            )
+
+        # Filtro por disponibilidade de agendamento
+        if recursoFiltro.agendamentos:
+            print("Filtro por agendamento -> ", len(recursoFiltro.agendamentos), " agendamentos desejados" )
+            iter_recursos = (r for r in iter_recursos
+                # Pega apenas os recursos cujo número de agendamentos sobrepostos seja 0
+                if
+                    [x for (x,y) in product(recursoFiltro.agendamentos,r.agendamentos) if not x.intervalo.intercede(y.intervalo)]
+                or not r.agendamentos
+                )
+
+        return list(iter_recursos)
+
+    def proximidade(self,lugar1,lugar2):
+        l1 = lugar1.split(',')
+        l2 = lugar2.split(',')
+        taxa = 1
+        for index,lugar in enumerate(l2):
+            if len(l1) == index:
+                break
+            taxa = taxa * SequenceMatcher(None, lugar.strip(), l1[index].strip()).ratio()
+        return taxa
 
     def todos(self):
         return self.recursos
--- web/app.py
+++ web/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, send_from_directory
 from domain import ServicoCRUDRecurso, Recurso, TipoRecurso
 from .RepositorioRecursoEmMemoria import RepositorioRecursoEmMemoria
+from .FormulariosDeRecurso import FormulariosDeRecurso
 App = Flask(__name__)
 
 # Instanciando adapters
@@ -8,11 +9,11 @@ repositorio_recurso = RepositorioRecursoEmMemoria()
 
 # Instanciando serviço hexagonal
 crud_recurso = ServicoCRUDRecurso(repositorio_recurso)
-
+form_recurso = FormulariosDeRecurso(repositorio_recurso)
 # Criando menu
 def gerarMenu():
     menu = [("Listar Recursos","/"),
-            ("Buscar Recurso","/recursos/buscar"),
+            ("Buscar Recurso","/recursos/buscar/"),
             ("Criar Novo Recurso","/recursos/novo")]
     return [(label,url,url == request.url_rule.rule) for (label,url) in menu]
 
@@ -27,38 +28,34 @@ def serve_stylesheet(path):
 @App.route("/js/<path>")
 def serve_script(path):
     return send_from_directory("./js",path)
-
-@App.route("/recursos/listar")
+# -----------------------------------------------
+# Paginas Recurso
+@App.route("/recursos/buscar/")
 def lista_recurso():
-    return render_template("recursos.html", recursos=crud_recurso.todos(), menu=gerarMenu())
+    return render_template("form_buscar_recurso.html", tipos_recurso=[(tipo.nome,tipo.id) for tipo in crud_recurso.metadados["tipos"]], menu=gerarMenu())
 
-@App.route("/recursos/listar", methods=["POST"])
-def lista_busca_recurso():
-    return render_template("recursos.html", recursos=crud_recurso.todos(), menu=gerarMenu())
+# UC01 - Buscar Recurso
+@App.route("/recursos/", methods=["GET"])
+def resultado_busca_recurso():
+    # try:
+    (id, nome, tipo, intervalos, local) = form_recurso.busca(request.args)
+    # except:
+    #     # TODO - Mensagens de erro para busca
+    #     return redirect("/recursos/buscar")
+    return render_template("recursos.html", recursos=crud_recurso.buscar(id, nome, tipo, intervalos, local), menu=gerarMenu())
 
-@App.route("/recursos/novo")
-def novo_recurso():
-    return render_template("form_recurso.html", tipos_recurso=[(tipo.nome,tipo.id) for tipo in crud_recurso.tipos()], recurso=Recurso(), menu=gerarMenu())
-
-@App.route("/recursos/buscar")
-def form_busca_recurso():
-    return render_template("form_buscar_recurso.html", tipos_recurso=[(tipo.nome,tipo.id) for tipo in crud_recurso.tipos()], menu=gerarMenu())
-
-@App.route("/recursos/buscar/", methods=["POST"])
-def form_busca_recurso_por_id():
-    # TODO - Melhore-me
-    if not request.form["id-recurso"]:
-        return redirect("/")
-    return redirect("/recurso/" + request.form["id-recurso"])
-
-@App.route("/recurso/<id>")
+@App.route("/recursos/<id>")
 def recurso(id):
     if not id:
         # TODO - Add 404
         return redirect("/recursos/buscar/")
     return render_template("recursos.html", recursos=crud_recurso.todos(), menu=gerarMenu())
 
-@App.route("/recursos", methods=["POST"])
+@App.route("/admin/recursos/novo")
+def novo_recurso():
+    return render_template("form_recurso.html", tipos_recurso=[(tipo.nome,tipo.id) for tipo in crud_recurso.tipos()], recurso=Recurso(), menu=gerarMenu())
+
+@App.route("/admin/recursos/novo", methods=["POST"])
 def criar_recurso():
     recurso = crud_recurso.criar(request.form)
     return redirect("/")
