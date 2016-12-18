8055e5dfd13d13f28a931511777bba3f587dbda7
Bernardo Amorim <contato@bamorim.com>
2016-12-18 17:46:14 -0200

    Condicionais no Template

--- web/autenticacao.py
+++ web/autenticacao.py
@@ -1,6 +1,7 @@
 from flask import session, current_app, g
 from functools import wraps
 from web.excecoes import ExcecaoNaoAutenticado
+from domain.usuario.nivel_acesso import Administrador
 
 def registrar_precarregar_usuario(app):
     @app.before_request
@@ -9,9 +10,11 @@ def registrar_precarregar_usuario(app):
         if concessao:
             g.usuario = current_app.autenticacao.usuario(concessao)
             g.nivelAcesso = current_app.autenticacao.nivel(concessao)
+            g.admin = g.nivelAcesso == Administrador()
         else:
             g.usuario = None
             g.nivelAcesso = None
+            g.admin = False
 
 def requer_usuario(f):
     """Decorador que levanta uma exceção caso o usuário não esteja logado"""
