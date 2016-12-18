2fe1a134ea9d396ffe0e9757b70071922c4f11df
Lucas <lucas.gomes@poli.ufrj.br>
2016-12-18 13:30:15 -0200

    Criando branch app-crud-usuario. Nenhum arquivo funcionando ainda

--- web/views/usuarios.py
+++ web/views/usuarios.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app, request, url_for, redirect
+from domain.usuario import Usuario, DTOUsuario
 
 view_usuarios = Blueprint('usuarios', __name__)
 
 @view_usuarios.route("/")
 def index():
-    return render_template("usuarios/index.html")
+    usuarios = current_app.crud_usuario.listar()
+    print(usuarios)
+    return render_template("usuarios/index.html", usuarios=usuarios)
 
 @view_usuarios.route("/novo")
 def novo():
@@ -12,4 +15,13 @@ def novo():
 
 @view_usuarios.route("/<id_usuario>")
 def detalhes(id_usuario):
+    return render_template("usuarios/detalhes.html")
+
+
+@view_usuarios.route("/showdetalhes")
+def showdetalhes():
+    return render_template("usuarios/detalhes.html")
+
+@view_usuarios.route("/<id_usuario>/editar")
+def editar(id_usuario):
     return render_template("usuarios/detalhes.html")
