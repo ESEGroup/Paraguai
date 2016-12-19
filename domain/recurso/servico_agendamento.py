05c7a2b01da2dcfa3759be157c8f6d9b65b14aea
Varlen Neto <varlenneto@poli.ufrj.br>
2016-12-19 13:47:34 -0200

    Adiciona Serviço para Agendamento

---
+++ domain/recurso/servico_agendamento.py
@@ -0,0 +1,54 @@
+from .recurso import Recurso
+from .tipo import TipoRecurso
+from .filtro import FiltroRecurso
+from domain.IntervaloDeTempo import IntervaloDeTempo
+from domain.agendamento import Agendamento
+
+class ServicoAgendamento():
+    def __init__(self, repositorio):
+        self.repositorio = repositorio
+
+    def listar(self):
+        return [
+          (agendamento,recurso)
+          for recurso in self.repositorio.listar()
+          for agendamento in recurso.agendamentos
+        ]
+
+    def listar_por_usuario(self, IDUsuario):
+        return [
+          (agendamento,recurso)
+          for recurso in self.repositorio.listar()
+          for agendamento in recurso.agendamentos
+          if agendamento.responsavel == IDUsuario
+        ]
+
+    def listar_por_recurso(self, IDRecurso):
+        recurso = self.repositorio.obter(IDRecurso)
+        return recurso.agendamentos
+
+    def agendar(self, IDRecurso, IDUsuario, intervalo):
+        recurso = self.repositorio.obter(IDRecurso)
+
+        # Verificar se o recurso já tem um agendamento sobreposto
+        for agendamento in recurso.agendamentos:
+            if intervalo.intercede(agendamento.intervalo):
+                raise ExcecaoAgendamentoRecursoOcupado
+        # Verificar se o intervalo de tempo bate com algum agendamento do usuário
+        agendamentos_usuario = self.listar_por_usuario(IDUsuario)
+        for agendamento_usuario,recurso_pai in agendamentos_usuario:
+            if intervalo.intercede(agendamento_usuario.intervalo):
+                raise ExcecaoAgendamentoUsuarioOcupado
+        # Verificar se o recurso está indisponível
+        if recurso.indisponivel:
+            raise ExcecaoAgendamentoRecursoIndisponivel
+        novo_agendamento = Agendamento(intervalo, IDUsuario)
+        recurso.agendamentos.append(novo_agendamento)
+
+    def remover(self, IDRecurso, intervalo):
+        recurso = self.repositorio.obter(IDRecurso)
+        recurso.agendamentos = [
+            agendamento
+            for agendamento in recurso.agendamentos
+            if not agendamento.intervalo.intercede(intervalo)
+        ]
